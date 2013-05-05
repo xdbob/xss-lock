29ca873c1be9aba3eb6f5566c66a8e513c41a766
Raymond Wagenmaker <raymondwagenmaker@gmail.com>
2013-05-05 15:16:59 +0200

    fix dim-screen script

--- doc/dim-screen.sh
+++ doc/dim-screen.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 min_brightness=0
 
-get_brighness() {
+get_brightness() {
+    xbacklight -get
+
+    # Or, for drivers without RandR backlight property support (e.g. radeon):
+    #cat /sys/class/backlight/acpi_video0/brightness
 }
 
 set_brightness() {
+    xbacklight -set $1
+
+    # Or, for drivers without RandR backlight property support (e.g. radeon):
+    #echo $1 | sudo tee /sys/class/backlight/acpi_video0/brightness
 }
 
-trap "set_brightness $(get_brightness); exit" TERM
-
+trap "set_brightness $(get_brightness); [[ \$pid ]] && kill \$pid" EXIT
+trap 'exit 0' TERM
+sleep 2147483647 & pid=$!
 set_brightness $min_brightness
+wait

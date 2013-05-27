d4fdbaf2533d804fa4924762ce57166c2bd26306
Raymond Wagenmaker <raymondwagenmaker@gmail.com>
2013-05-27 22:41:05 +0200

    suggest tmpfiles.d solution instead of `sudo tee` for sysfs backlight

--- doc/dim-screen.sh
+++ doc/dim-screen.sh
@@ -16,7 +16,13 @@ set_brightness() {
     xbacklight -set $1
 
     # Or, for drivers without RandR backlight property support (e.g. radeon):
-    #echo $1 | sudo tee /sys/class/backlight/acpi_video0/brightness
+    #echo $1 > /sys/class/backlight/acpi_video0/brightness
+    #
+    # To make this work, create a .conf file in /etc/tmpfiles.d/ containing the
+    # following line to make the sysfs file writable for group 'users' (change
+    # the last - to a number to set the initial brightness):
+    #
+    #     f /sys/class/backlight/acpi_video0/brightness 0664 root users - -
 }
 
 trap 'exit 0' TERM
